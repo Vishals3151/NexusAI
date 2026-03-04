d7b5871b8c3520a74acdb1bc495dad60047ab531
vishal gaikwad <vishalsureshgaikwad115@gmail.com>
2026-03-04 23:40:48 +0530

    Agent data table

---
+++ src/modules/agents/ui/components/columns.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { AgentGetOne } from "../../types";
+import { Badge } from "@/components/ui/badge";
+import { GeneratedAvatar } from "@/components/generated-avatar";
+import { CornerDownRightIcon, VideoIcon } from "lucide-react";
+
+export const columns: ColumnDef<AgentGetOne>[] = [
+  {
+    accessorKey: "name",
+    header: "Agent name",
+    cell: ({ row }) => {
+      return (
+        <div className="flex flex-col gap-y-1">
+          <div className="flex items-center gap-x-2">
+            <GeneratedAvatar
+              variant="butttsNeutral"
+              seed={row.original.name}
+              className="size-6"
+            />
+            <span className="font-semibold capitalize">
+              {row.original.name}
+            </span>
+          </div>
+
+          <div className="flex items-center gap-x-2">
+            <CornerDownRightIcon className="size-3 text-muted-foreground" />
+            <span className="text-sm text-muted-foreground max-w-[200px] truncate capitalize">
+              {row.original.instructions}
+            </span>
+          </div>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "meetingCount",
+    header: "Meetings",
+    cell: () => {
+      return (
+        <Badge
+          variant="outline"
+          className="flex items-center gap-x-2 [&>svg]:size-4"
+        >
+          <VideoIcon className="text-blue-700" />
+          5 Meetings
+        </Badge>
+      );
+    },
+  },
+];
