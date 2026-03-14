721de4ee1fb4c2a8cfeb24d8dc61bf42cd966ea1
vishal gaikwad <vishalsureshgaikwad115@gmail.com>
2026-03-14 21:13:15 +0530

    Agent Page

--- src/modules/agents/ui/views/agent-view.tsx
+++ src/modules/agents/ui/views/agent-view.tsx
@@ -3,7 +3,7 @@
 import { LoadingState } from "@/components/loading-state";
 import { ErrorState } from "@/components/error-state";
 import { useTRPC } from "@/trpc/client";
-
+import { useRouter } from "next/navigation";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { DataTable } from "../components/data-table";
 import { columns } from "../components/columns";
@@ -12,6 +12,7 @@ import { useAgentFilters } from "../../hooks/use-agents-filters";
 import { DataPagination } from "../components/data-pagination";
 
 export const AgentsView = () => {
+  const router = useRouter();
   const [filters, setFilters] = useAgentFilters();
 
   const trpc = useTRPC();
@@ -23,7 +24,11 @@ export const AgentsView = () => {
 
   return (
     <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-      <DataTable data={data.items} columns={columns} />
+      <DataTable
+        data={data.items}
+        columns={columns}
+        onRowClick={(row) => router.push(`/agents/${row.id}`)}
+      />
       <DataPagination
         page={filters.page}
         totalPages={data.totalPages}
